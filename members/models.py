9dcf9be59d4417ce3e9aa9650118023bb2598528
GrandpaB <sbentley1302@gmail.com>
2026-02-12 13:47:31 +1100

    created members_member table in DB

--- members/models.py
+++ members/models.py
@@ -1,4 +1,6 @@
 from django.db import models
-null=True
 
 # Create your models here.
+class Member(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
