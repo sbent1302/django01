e8806c012039b0cfc9643d95ae347ee214ae0ab0
GrandpaB <sbentley1302@gmail.com>
2026-02-24 17:20:47 +1100

    Query sets setup for testing view

--- members/views.py
+++ members/views.py
@@ -23,8 +23,20 @@ def main(request):
   return HttpResponse(template.render())
   
 def testing(request):
+  # This is how to get all the data from the database, and pass it to the template
+  mymembers = Member.objects.all().values()
+
+  # Following are filter examples, to show how to filter the data
+  # mymembers = Member.objects.filter(firstname='Emil').values()
+  # mymembers = Member.objects.filter(lastname='Refsnes', id=2).values()
+  # mymembers = Member.objects.filter(firstname__startswith='L').values()
+
+  # Following is how to order by firstname
+  # mymembers = Member.objects.all().order_by('firstname').values()
+  # mymembers = Member.objects.all().order_by('firstname','-lastname').values()
+  
   template = loader.get_template('template.html')
   context = {
-    'fruits': ['Apple', 'Banana', 'Cherry'],   
+    'mymembers': mymembers,
   }
   return HttpResponse(template.render(context, request))
